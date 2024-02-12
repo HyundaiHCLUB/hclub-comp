b5377492169f962c625e1e4c8a754e325be5454f
smartpodo@kakao.com <smartpodo@kakao.com>
2024-02-12 21:19:28 +0900

    fix: rename

--- src/main/java/site/hclub/hyndai/service/CompServiceImpl.java
+++ src/main/java/site/hclub/hyndai/service/CompServiceImpl.java
@@ -3,5 +3,5 @@ package site.hclub.hyndai.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CompServcieImpl implements CompService{
+public class CompServiceImpl implements CompService {
 }
