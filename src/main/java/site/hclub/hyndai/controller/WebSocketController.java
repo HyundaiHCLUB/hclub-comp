32430b198d7eb3fd8c97ae5c8e4804450d68c41f
hyeyeon <tjsalasu@naver.com>
2024-02-26 12:26:34 +0900

    [BE] fix: RedisConfig 수정

--- src/main/java/site/hclub/hyndai/controller/WebSocketController.java
+++ src/main/java/site/hclub/hyndai/controller/WebSocketController.java
@@ -1,28 +1,27 @@
-//package site.hclub.hyndai.controller;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.stereotype.Controller;
-//import site.hclub.hyndai.domain.Team;
-//import site.hclub.hyndai.dto.request.MatchingRequest;
-//import site.hclub.hyndai.service.MatchingService;
-//
-//@Controller
-//public class WebSocketController {
-//
-//    @Autowired
-//    private MatchingService matchingService;
-//
-//    @Autowired
-//    private SimpMessagingTemplate messagingTemplate;
-//    @MessageMapping("/addTeamToQueue")
-//    public void addTeamToQueue(MatchingRequest matchingRequest) {
-//        matchingService.addToQueue(matchingRequest);
-//    }
-//
-//
-//    @MessageMapping("/topic/matches")
-//    public void sendMatchResult(String matchResult) {
-//        messagingTemplate.convertAndSend("/topic/matches", matchResult);
-//    }
-//}
+package site.hclub.hyndai.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import site.hclub.hyndai.dto.request.MatchingRequest;
+import site.hclub.hyndai.service.MatchingService;
+
+@Controller
+public class WebSocketController {
+
+    @Autowired
+    private MatchingService matchingService;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+    @MessageMapping("/addTeamToQueue")
+    public void addTeamToQueue(MatchingRequest matchingRequest) {
+        matchingService.addToQueue(matchingRequest);
+    }
+
+
+    @MessageMapping("/topic/matches")
+    public void sendMatchResult(String matchResult) {
+        messagingTemplate.convertAndSend("/topic/matches", matchResult);
+    }
+}
