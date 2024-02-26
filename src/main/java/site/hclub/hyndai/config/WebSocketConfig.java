1e05173291cfb740ddfe00d9d4ec5ef86c916d43
codeSweet0828 <codesweet0828@gmail.com>
2024-02-26 11:15:14 +0900

    소켓 설정 어노테이션 추가

--- src/main/java/site/hclub/hyndai/config/WebSocketConfig.java
+++ src/main/java/site/hclub/hyndai/config/WebSocketConfig.java
@@ -6,7 +6,17 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 import site.hclub.hyndai.service.SocketHandler;
 
-@Configuration
+/**
+ * @author 김은솔
+ * @description: 웹소켓을 위한 Configuration 설정
+ * ===========================
+	   AUTHOR      NOTE
+ * ---------------------------
+ *    김은솔         최초생성
+ * ===========================
+ */
+@Configuration 
+@EnableWebSocket 
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
     private final SocketHandler webSocketHandler;
@@ -26,6 +36,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
         registry.addEndpoint("/ws").withSockJS();
     }
 
+    /**
+	 작성자: 김은솔 
+	 처리 내용:   WebSocketHandler 요청 매핑 을 구성한다.
+	*/
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
