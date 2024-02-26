804dad9a6888924958995217182f315988175cd8
hyeyeon <tjsalasu@naver.com>
2024-02-26 14:48:19 +0900

    [BE] fix: RedisConfig 수정

--- src/main/java/site/hclub/hyndai/config/RedisConfig.java
+++ src/main/java/site/hclub/hyndai/config/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -19,11 +19,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("localhost");
-        jedisConnectionFactory.setPort(6379);
-        jedisConnectionFactory.setPassword(password);
-        return jedisConnectionFactory;
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName("localhost");
+        lettuceConnectionFactory.setPort(6379);
+        lettuceConnectionFactory.setPassword(password);
+        return lettuceConnectionFactory;
     }
 
     @Bean
@@ -37,4 +37,3 @@ public class RedisConfig {
         return redisTemplate;
     }
 }
-
