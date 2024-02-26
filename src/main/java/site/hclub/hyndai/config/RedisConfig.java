80911b08f450f7d007858898905fba30178b66ae
smartpodo@kakao.com <smartpodo@kakao.com>
2024-02-26 16:20:45 +0900

    fix: lettuce -> jedis redis & comp detail

--- src/main/java/site/hclub/hyndai/config/RedisConfig.java
+++ src/main/java/site/hclub/hyndai/config/RedisConfig.java
@@ -3,8 +3,7 @@ package site.hclub.hyndai.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -17,23 +16,22 @@ public class RedisConfig {
     @Value("${SPRING-DATA-REDIS-PASSWORD}")
     private String password;
 
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
-//        lettuceConnectionFactory.setHostName(address);
-        lettuceConnectionFactory.setPort(6379);
-        lettuceConnectionFactory.setPassword(password);
-        return lettuceConnectionFactory;
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("localhost");
+        connectionFactory.setPort(6379);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-
+        redisTemplate.setConnectionFactory(connectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-
         return redisTemplate;
     }
+
 }
